959a2b6bc447c1775ecfd6406bae5250906c4545
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2004-07-27 16:26:08 +0000

    Added docs to the script

--- DPD/mkhash2.sh
+++ DPD/mkhash2.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
+#
+# Script to create a file containing md5sum and filenames of each file
+# found in <source dir> and <dest dir>
+# Example:
+#   mkhash2.sh Movies1/ Movies2/
+# 
+# It will create 2 files, source.hash and dest.hash
+#
+# dpd can process this files and synchronize both <source dir> and <dest dir>
+# The -music flag should be used if you want to compare directories containing
+# music files
+#
+
 SOURCE="$1"
 DEST="$2"
 VIDEO_EXT="\.avi|\.mpg|\.mpeg|\.divx|\.xvid|\.wmv"
 MUSIC_EXT="\.mp3|\.ogg"
 EXT=$VIDEO_EXT
-case $1 in
-    -music)
-	EXT=$MUSIC_EXT
-	SOURCE=$2
-	DEST=$3
-	;;
-    *)
-	;;
-esac
 
-find "$SOURCE" | grep -E $EXT$ |
-while read line
-do
-    md5=`head -c 512k "$line" | md5sum | sed -e 's/-$//'`
-    echo "$md5 $line" >> source.hash
-done
+#FIXME: check arguments properly
+if test -z $1 || test -z $2; then
+    echo "Usage: mkhash2.sh [-music] <source dir> <dest dir>"
 
-find "$DEST" | grep -E $EXT$ |
-while read line
-do
-    md5=`head -c 512k "$line" | md5sum | sed -e 's/-$//'`
-    echo "$md5 $line" >> dest.hash
-done
+else
+    case $1 in
+	-music)
+	    EXT=$MUSIC_EXT
+	    SOURCE=$2
+	    DEST=$3
+	    ;;
+	*)
+	    ;;
+    esac
+
+    find "$SOURCE" | grep -E $EXT$ |
+    while read line
+    do
+	md5=`head -c 512k "$line" | md5sum | sed -e 's/-$//'`
+	echo "$md5 $line" >> source.hash
+    done
+
+    find "$DEST" | grep -E $EXT$ |
+    while read line
+    do
+	md5=`head -c 512k "$line" | md5sum | sed -e 's/-$//'`
+	echo "$md5 $line" >> dest.hash
+    done
+fi
