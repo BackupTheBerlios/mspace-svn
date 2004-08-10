10b0e8a9ede048ccb0017fc0210ed98fce8aa8c4
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2004-08-10 21:57:34 +0000

    New Action enabled custom widgets

---
+++ muine/src/Muine/Gtk.Ext/ImageButton.cs
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2004 Sergio Rubio <sergio.rubio@hispalinux.es>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+namespace Gtk.Ext {
+    
+    using Gtk;
+    using System;
+    
+    public class ImageButton : ActionButton
+    {
+	private Image img;
+	private HBox box = new HBox ();
+
+	public ImageButton (UIAction action) : base (action)
+	{
+	    image = new Image ();
+	    image.SetFromStock (action.StockIcon, IconSize.LargeToolbar);
+	    InitComponent ();
+	}
+
+	private Image image;
+	public Image Image
+	{
+	    get {
+		return image;
+	    }
+	    set {
+		image.Destroy ();
+		image = value;
+		box.PackEnd (image);
+	    }
+	}
+
+	private bool textEnabled = false;
+	public bool TextEnabled {
+	    get {
+		return textEnabled;
+	    }
+	    set {
+		textEnabled = value;
+		box.PackEnd (new Label (action.Label));
+	    }
+	}
+
+	private void InitComponent ()
+	{
+	    if (textEnabled)
+		box.PackEnd (new Label (action.Label));
+	    box.PackStart (image);
+	    //Child.Destroy ();
+	    Child = box;
+	    box.Show ();
+	}
+    }
+    
+}
+
