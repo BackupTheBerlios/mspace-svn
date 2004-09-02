15eff97455fdb2a72ea9f5bbad451ae1492d47b2
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2004-09-02 16:19:22 +0000

    better makesources fixed makefile

--- Muine.Sdk/src/makesources.sh
+++ Muine.Sdk/src/makesources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-echo 'SOURCES = \' > sources.list
-for i in `find ./ -iname '*.cs'`; do echo $i\\ >> sources.list ;done
+echo CSSOURCES= > sources.list
+for i in `find ./ -iname '*.cs'`; do echo CSSOURCES += $i >> sources.list ;done
 grep -v -e 'tests' sources.list > sources.list.fixed
 mv sources.list.fixed sources.list
