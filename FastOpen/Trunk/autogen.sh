3c9b2b49ec165927d38b5f8db827a7a388368572
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2004-09-15 21:16:54 +0000

    Fixed makefile and autogen spanish readme no longer mantained

--- FastOpen/Trunk/autogen.sh
+++ FastOpen/Trunk/autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="muine"
+PKG_NAME="FastOpen"
 
 (test -f $srcdir/configure.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
