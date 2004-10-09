83f0996007cc92c7b861428ee4a701b96ce1767f
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2004-10-09 12:30:21 +0000

    Added trunk branch

---
+++ Chicken/Trunk/src/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly:AssemblyVersion("0.1.1")]
+[assembly:AssemblyTitle("Chicken Framework")]
+[assembly:AssemblyDelaySign(false)]
+//[assembly:AssemblyKeyFile("gtk-sharp.snk")]
