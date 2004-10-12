1a1193959d6e00892f5ff3a2a88b436ba792c96c
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2004-10-12 12:22:22 +0000

    old branc

--- Chicken/branches/Chicken-0.1.3/src/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly:AssemblyVersion("0.1.1")]
-[assembly:AssemblyTitle("Chicken Framework")]
-[assembly:AssemblyDelaySign(false)]
-//[assembly:AssemblyKeyFile("gtk-sharp.snk")]
