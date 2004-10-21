324cfaf00edbef568b9929328fe796cd06d4a7c1
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2004-10-21 15:11:02 +0000

    2004-10-21  Sergio Rubio  <sergio.rubio@hispalinux.es>
            * Makefile.am: Cleaned
    	* monkeypopd/Client.cs: Added Application.Init,Run calls

--- monkeypop/trunk/monkeypopd/Client.cs
+++ monkeypop/trunk/monkeypopd/Client.cs
@@ -1,16 +1,20 @@
 namespace Foo
 {
-  using System;
-  using DBus;
+	using System;
+	using DBus;
+	using System.Runtime.InteropServices;
 
-  public class Client
-  {
-    public static void Main(string [] args)
-    {
-      Connection connection = Bus.GetSessionBus();
-      Service service = Service.Get(connection, "org.rubiojr.MonkeyPop");
-      NotificationDaemon daemon = (NotificationDaemon) 
-      		service.GetObject(typeof(NotificationDaemon), "/org/rubiojr/MonkeyPop/NotificationDaemon");
-    }
-  }
+	public class Client
+	{
+		[DllImport ("dbus-glib-1")]
+		private extern static void dbus_g_thread_init ();
+		public static void Main(string [] args)
+		{
+			dbus_g_thread_init ();
+			Connection connection = Bus.GetSessionBus();
+			Service service = Service.Get(connection, "org.rubiojr.MonkeyPop");
+			NotificationDaemon daemon = (NotificationDaemon) 
+				service.GetObject(typeof(NotificationDaemon), "/org/rubiojr/MonkeyPop/NotificationDaemon");
+		}
+	}
 }
