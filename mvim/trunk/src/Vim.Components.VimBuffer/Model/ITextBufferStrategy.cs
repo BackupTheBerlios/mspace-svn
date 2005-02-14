eddd12457713d8ca3de7ddd4c2f5fde19d95ba06
nestor <nestor@52acd38f-94df-0310-9187-83e47fefcd02>
2005-02-14 22:58:24 +0000

    Added sample GapImplementation from SharpDevelop. Check algorithmy.

---
+++ mvim/trunk/src/Vim.Components.VimBuffer/Model/ITextBufferStrategy.cs
@@ -0,0 +1,12 @@
+
+namespace Vim.Components.VimBuffer.Model {
+    public interface ITextBufferStrategy {
+        int Length {get;}
+        void Insert (int offset, string text);
+        void Remove (int offset, int length);
+        void Replace (int offset,int length, string text);
+        string GetText (int offset, int length);
+        char GetCharAt (int offset);
+        void SetContent (string text);
+    }
+}
--- mvim/trunk/src/Vim.Components.VimBuffer/Model/IVimBuffer.cs
+++ mvim/trunk/src/Vim.Components.VimBuffer/Model/IVimBuffer.cs
@@ -1,5 +1,18 @@
+//
+// Vim.Components.VimBuffer.Model.IVimBuffer.cs :  
+//
+// Author:
+//      Néstor Salceda Alonso (wizito@gentelibre.org)
+// (C) 2005
+
 namespace Vim.Components.VimBuffer.Model {
+    //
+    //<summary>
+    //  We try use same Buffer as SharpDevelop do.
+    //  Try use a Gap Buffer.
+    //</summary>
+    //
     public interface IVimBuffer {
-        string Text {get; set;}
+         
     }
 }
