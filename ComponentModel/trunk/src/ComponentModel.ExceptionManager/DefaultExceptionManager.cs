3abddf8addc2318920dc8f722ab06572d6f9c729
nestor <nestor@52acd38f-94df-0310-9187-83e47fefcd02>
2005-04-04 09:21:46 +0000

    Refactor.

---
+++ ComponentModel/trunk/src/ComponentModel.ExceptionManager/DefaultExceptionManager.cs
@@ -0,0 +1,10 @@
+using System;
+using ComponentModel.Interfaces;
+
+namespace ComponentModel.ExceptionManager {
+    public class DefaultExceptionManager : IExceptionManager {
+        public virtual void ProcessException (Exception exception) {
+            Console.WriteLine (exception.ToString ());
+        }
+    }
+}
