5a3b588a4710c4d1d08bfa4e44178e9839e69b59
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2005-05-05 07:23:02 +0000

    * Some cosmetic changes and fixes * Added a README

--- servicemenus/uploadToServer/uploadToServer.sh
+++ servicemenus/uploadToServer/uploadToServer.sh
@@ -2,12 +2,14 @@
 
 FILEPATH=$1
 FILENAME=$2
+SERVER_URL="fish://rubiojr.no-ip.org/home/rubiojr/public_html/Download/"
+DOWNLOAD_URL="http://rubiojr.no-ip.org/~/rubiojr/Download/$FILENAME"
 
 ## Copy the file to the server using kfmclient
-kfmclient copy $FILEPATH fish://rubiojr.no-ip.org/home/rubiojr/public_html/Download/
+kfmclient copy $FILEPATH $SERVERURL
 
 ## Set the URL in clipboard
-dcop klipper klipper setClipboardContents "http://rubiojr.no-ip.org/~rubiojr/Download/$FILENAME"
+dcop klipper klipper setClipboardContents "$DOWNLOAD_URL"
 
 ## Notify when finished
-dcop knotify Notify notify "URL received" "<b>Web Uploader</b>" "Download URL <b><font color='red'>ready</font></b> in klipper" "" "" 16 0
+dcop knotify Notify notify "URL received" "<b>File Uploader</b>" "Download URL <b><font color='red'>ready</font></b> in klipper" "" "" 16 0
