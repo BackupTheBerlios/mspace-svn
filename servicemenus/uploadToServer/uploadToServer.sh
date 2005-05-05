96240141d2534ed233fee21874f13ce2ff5691e2
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2005-05-05 07:25:17 +0000

    grrrr. More fixes

--- servicemenus/uploadToServer/uploadToServer.sh
+++ servicemenus/uploadToServer/uploadToServer.sh
@@ -3,10 +3,10 @@
 FILEPATH=$1
 FILENAME=$2
 SERVER_URL="fish://rubiojr.no-ip.org/home/rubiojr/public_html/Download/"
-DOWNLOAD_URL="http://rubiojr.no-ip.org/~/rubiojr/Download/$FILENAME"
+DOWNLOAD_URL="http://rubiojr.no-ip.org/~rubiojr/Download/$FILENAME"
 
 ## Copy the file to the server using kfmclient
-kfmclient copy $FILEPATH $SERVERURL
+kfmclient copy $FILEPATH $SERVER_URL
 
 ## Set the URL in clipboard
 dcop klipper klipper setClipboardContents "$DOWNLOAD_URL"
