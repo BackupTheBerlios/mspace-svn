567893a3ed87dc40c50d8e2195c6dc25ef84060a
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2005-05-16 15:39:01 +0000

    defaults to audio bitrate = 64

--- scripts/encode-ppc.rb
+++ scripts/encode-ppc.rb
@@ -13,7 +13,7 @@ end
 destfile = "PocketPC_" + File.basename(file)
 cmd = "mencoder"
 ## PocketDivxEncoder args for PocketPC ##
-pocketpc_opts = "-srate 22050 -oac mp3lame -lameopts mode=2:cbr:br=24 -vf scale=320:240,eq2=1.0:1.0:0.03:1.0 -sws 2 -ovc lavc -lavcopts vcodec=mpeg4:vhq:vbitrate=248 -ffourcc DX50"
+pocketpc_opts = "-srate 22050 -oac mp3lame -lameopts mode=2:cbr:br=64 -vf scale=320:240,eq2=1.0:1.0:0.03:1.0 -sws 2 -ovc lavc -lavcopts vcodec=mpeg4:vhq:vbitrate=248 -ffourcc DX50"
 lame_args = "#{file} -noskip -vf scale=320:240 -of mpeg -ovc lavc -lavcopts vcodec=mpeg1video:vbitrate=400000:vrc_minrate=100:vrc_maxrate=500:vrc_buf_size=300:vpass=1 -oac copy -o #{destfile}"
 
 puts "Encoding #{file} to #{destfile}"
