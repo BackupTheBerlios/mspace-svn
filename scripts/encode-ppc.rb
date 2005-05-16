b3d2f6f1d3aa1b0150cc0fbefd91d5f959a06b92
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2005-05-16 13:07:31 +0000

    crap removed

--- scripts/encode-ppc.rb
+++ scripts/encode-ppc.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/ruby
-require "shell"
 
 file = ARGV[0]
 if !file
@@ -9,8 +8,6 @@ end
 destfile = "PocketMovie_" + File.basename(file)
 cmd = "mencoder"
 args = "#{file} -aid 129 -noskip -vf scale=320:240 -of mpeg -ovc lavc -lavcopts vcodec=mpeg1video:vbitrate=400000:vrc_minrate=100:vrc_maxrate=500:vrc_buf_size=300:vpass=1 -oac lavc -lavcopts acodec=mp2:abitrate=96 -o #{destfile}"
-
-Shell.def_system_command(cmd, args)
 
 puts "Encoding #{file} to #{destfile}"
 `#{cmd} #{args}`
