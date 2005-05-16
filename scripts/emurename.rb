c8eae977c68e084671550b8d0ca8338d02d0d6e9
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2005-05-16 21:02:05 +0000

    New script to rename the files with spaces, brackets, etc.

---
+++ scripts/emurename.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+require "fileutils"
+
+def usage
+	puts "Usage: #{__FILE__} <directory> | <file>"
+end
+
+def normalizeDir(dir)
+	Dir.foreach(dir) { |file|
+		return if File.directory?(dir)
+		normalizeFile(dir + "/" + file)
+	}
+end
+
+def normalizeFile(file)
+	ext = file[file.rindex(".") .. file.length]
+	basename = File.basename(file, ext)
+	dir = File.dirname(file)
+	return if basename == "." or basename == ".."
+	newName = dir + "/" + basename.scan(/\w+/).join("_") + ext
+	#The file does not need to be renamed
+	return if File.exist?(newName)
+	FileUtils.mv(file, newName)
+end
+
+if $0 == __FILE__
+	arg = ARGV[0]
+	usage if !arg
+	if File.directory?(arg)
+		normalizeDir(arg)
+	else
+		normalizeFile(arg)
+	end
+end
