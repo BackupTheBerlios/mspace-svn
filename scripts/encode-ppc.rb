7e7cd09414025eed7e5dd502af22d163e0e8b973
rubiojr <rubiojr@52acd38f-94df-0310-9187-83e47fefcd02>
2005-05-16 13:27:47 +0000

    changed some params

--- scripts/encode-ppc.rb
+++ scripts/encode-ppc.rb
@@ -1,13 +1,19 @@
 #!/usr/bin/ruby
 
+def usage
+	puts "Usage: encode-ppc.rb <file>"
+end
+
 file = ARGV[0]
 if !file
-	puts "no args"
+	usage
 	exit
 end
+
 destfile = "PocketMovie_" + File.basename(file)
 cmd = "mencoder"
-args = "#{file} -aid 129 -noskip -vf scale=320:240 -of mpeg -ovc lavc -lavcopts vcodec=mpeg1video:vbitrate=400000:vrc_minrate=100:vrc_maxrate=500:vrc_buf_size=300:vpass=1 -oac lavc -lavcopts acodec=mp2:abitrate=96 -o #{destfile}"
+#extraopts -aid 129
+args = "#{file} -noskip -vf scale=320:240 -of mpeg -ovc lavc -lavcopts vcodec=mpeg1video:vbitrate=400000:vrc_minrate=100:vrc_maxrate=500:vrc_buf_size=300:vpass=1 -oac copy -o #{destfile}"
 
 puts "Encoding #{file} to #{destfile}"
 `#{cmd} #{args}`
