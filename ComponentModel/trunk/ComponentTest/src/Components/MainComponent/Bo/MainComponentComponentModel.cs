ddb12f46da4b8c2648acf5ec1a6214a820fc2166
nestor <nestor@52acd38f-94df-0310-9187-83e47fefcd02>
2005-09-26 23:31:32 +0000

    Eliminado esto de aqui.
    Hare un componente para realizar los tests como debe hacerse.

--- ComponentModel/trunk/ComponentTest/src/Components/MainComponent/Bo/MainComponentComponentModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using ComponentModel;
-
-namespace ComponentModel.ComponentTest.Components.MainComponent.Bo {
-    [Component ("MainComponent", "ComponentModel.ComponentTest.Components.MainComponent.Exception.MainComponentExceptionManager")]
-    public class MainComponentComponentModel : DefaultComponentModel {
-    }
-}
